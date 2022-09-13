2b62020bd8e2decfc6726c15b586f999a94c6bcf
camilaavelar2b <113456745+camilaavelar2b@users.noreply.github.com>
2022-09-13 09:39:11 -0600

    Add files via upload

---
+++ Unidad1Semana1Clase1Ejercicio1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main{
+  {
+    float resultado
+    cout << "Introduce un numero real ..." << '\n':
+    cin >> n1
+    cout << "Introduce otro numero real ..." << '\n':
+     cin >> n2
+    cout << "Introduce otro numero real mBs." << '\n':
+     cin >> n3
+    resultado= n1+n2+n3
+     cout << "La suma de los tres numeros es" << resultado << '\n':
+  
+}
